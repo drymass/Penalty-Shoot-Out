ad770fae2c438b80f590fe12ab54cadec34634e1
drymass <124876608+drymass@users.noreply.github.com>
2023-02-09 17:40:57 +0800

    Create Penaltyshot.js

---
+++ Penaltyshot.js
@@ -0,0 +1,21 @@
+const startButton = document.getElementById("start");
+const restartButton = document.getElementById("restart");
+const directionInput = document.getElementById("direction");
+const resultDisplay = document.getElementById("result");
+
+startButton.addEventListener("click", function() {
+// Generate a random number for the goalkeeper direction
+const goalkeeperDirection = Math.floor(Math.random() * 9) + 1;
+// Compare the user selection with the goalkeeper direction
+if (directionInput.value == goalkeeperDirection) {
+resultDisplay.innerHTML = "Goal!";
+} else {
+resultDisplay.innerHTML = "Missed.";
+}
+});
+
+restartButton.addEventListener("click", function() {
+// Reset the result display and input
+resultDisplay.innerHTML = "";
+directionInput.value = "";
+});
