71f3baab2fc99dc451de97b6be765969ae4102af
drymass <124876608+drymass@users.noreply.github.com>
2023-02-09 22:51:16 +0800

    Update Penaltyshot.js

--- Penaltyshot.js
+++ Penaltyshot.js
@@ -1,21 +1,26 @@
+let goalkeeperDirection = Math.floor(Math.random() * 9) + 1;
+let shotDirection = 0;
+
 const startButton = document.getElementById("start");
 const restartButton = document.getElementById("restart");
 const directionInput = document.getElementById("direction");
 const resultDisplay = document.getElementById("result");
+const goalkeeper = document.getElementById("goalkeeper");
+const player = document.getElementById("player");
+const gantry = document.getElementById("gantry");
 
-startButton.addEventListener("click", function() {
-// Generate a random number for the goalkeeper direction
-const goalkeeperDirection = Math.floor(Math.random() * 9) + 1;
-// Compare the user selection with the goalkeeper direction
-if (directionInput.value != goalkeeperDirection) {
-resultDisplay.innerHTML = "Goal! The goalkeeper selected direction " + goalkeeperDirection;
+startButton.addEventListener("click", function () {
+shotDirection = parseInt(directionInput.value);
+if (shotDirection === goalkeeperDirection) {
+resultDisplay.innerHTML = "Miss";
+goalkeeper.src = "GoalkeeperImage" + goalkeeperDirection + ".jpg";
 } else {
-resultDisplay.innerHTML = "Missed.";
+resultDisplay.innerHTML = "Goal! The goalkeeper selected direction " + goalkeeperDirection;
+player.src = "PlayerImage.jpg";
+gantry.src = "GantryImage.jpg";
 }
 });
 
-restartButton.addEventListener("click", function() {
-// Reset the result display and input
-resultDisplay.innerHTML = "";
-directionInput.value = "";
+restartButton.addEventListener("click", function () {
+location.reload();
 });
