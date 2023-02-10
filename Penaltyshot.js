fa43d1df00f7ad5212f575eef690466f76779a68
drymass <124876608+drymass@users.noreply.github.com>
2023-02-10 09:40:49 +0800

    Update Penaltyshot.js

--- Penaltyshot.js
+++ Penaltyshot.js
@@ -1,16 +1,30 @@
-const resultDiv = document.querySelector("#result");
-const goalkeeperDirection = Math.floor(Math.random() * 9) + 1;
+document.addEventListener("DOMContentLoaded", function() {
+let readyButton = document.getElementById("readyButton");
+let game = document.getElementById("game");
+let arrows = document.getElementsByClassName("arrow");
+let goalkeeper = document.getElementById("goalkeeper");
+let player = document.getElementById("player");
+let result = document.getElementById("result");
+let directions = ["upleft", "up", "upright", "left", "center", "right", "downleft", "down", "downright"];
 
-arrows.forEach(arrow => {
-arrow.addEventListener("click", e => {
-let playerDirection = e.target.id.slice(-1);
-let result;
-if (playerDirection == goalkeeperDirection) {
-result = "miss";
-} else {
-result = "goal";
-}
-resultDiv.textContent = result;
-resultDiv.style.display = "block";
+readyButton.addEventListener("click", function() {
+game.style.display = "flex";
+readyButton.style.display = "none";
 });
+
+for (let i = 0; i < arrows.length; i++) {
+arrows[i].addEventListener("click", function() {
+let randomIndex = Math.floor(Math.random() * directions.length);
+let shotDirection = directions[i];
+let saveDirection = directions[randomIndex];
+
+if (shotDirection === saveDirection) {
+result.innerHTML = `Miss! The goalkeeper saved the shot to ${saveDirection}. Keep trying!`;
+result.style.display = "block";
+} else {
+result.innerHTML = `Goal! Congratulations! The goalkeeper couldn't save the shot to ${shotDirection}.`;
+result.style.display = "block";
+}
+});
+}
 });
