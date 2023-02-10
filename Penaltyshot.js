61db494f5da48b8e7e7e62fba82f2c602766e239
drymass <124876608+drymass@users.noreply.github.com>
2023-02-10 14:58:01 +0800

    Update Penaltyshot.js

--- Penaltyshot.js
+++ Penaltyshot.js
@@ -1,86 +1,36 @@
-document.getElementById("readyButton").addEventListener("click", function() {
-document.getElementById("game").style.display = "flex";
-document.getElementById("result").style.display = "block";
-document.getElementById("readyButton").style.display = "none";
+document.addEventListener("DOMContentLoaded", function() {
+var readyButton = document.getElementById("readyButton");
+var game = document.getElementById("game");
+var arrows = document.querySelectorAll("#arrows img");
+var result = document.getElementById("result");
+var chances = 5;
+var goals = 0;
+var misses = 0;
 
-let chances = 5;
-let goals = 0;
-let misses = 0;
+readyButton.addEventListener("click", function() {
+readyButton.style.display = "none";
+game.style.display = "flex";
 
-document.getElementById("arrow1").addEventListener("click", function() {
-play("upleft");
-});
-document.getElementById("arrow2").addEventListener("click", function() {
-play("up");
-});
-document.getElementById("arrow3").addEventListener("click", function() {
-play("upright");
-});
-document.getElementById("arrow4").addEventListener("click", function() {
-play("left");
-});
-document.getElementById("arrow5").addEventListener("click", function() {
-play("center");
-});
-document.getElementById("arrow6").addEventListener("click", function() {
-play("right");
-});
-document.getElementById("arrow7").addEventListener("click", function() {
-play("downleft");
-});
-document.getElementById("arrow8").addEventListener("click", function() {
-play("down");
-});
-document.getElementById("arrow9").addEventListener("click", function() {
-play("downright");
-});
-
-function play(playerChoice) {
+arrows.forEach(function(arrow) {
+arrow.addEventListener("click", function() {
 if (chances > 0) {
-let goalkeeperChoice = getGoalkeeperChoice();
-document.getElementById("result").innerHTML =
-"Goalkeeper chose: " + goalkeeperChoice;
-if (playerChoice === goalkeeperChoice) {
-misses++;
+var goalkeeper = Math.floor(Math.random() * 9) + 1;
+var player = parseInt(this.id.replace("arrow", ""));
+
+if (goalkeeper === player) {
+result.innerHTML = "Miss! The goalkeeper jumps to the " + player + " direction.";
+misses += 1;
 } else {
-goals++;
+result.innerHTML = "Goal! The goalkeeper jumps to the " + goalkeeper + " direction.";
+goals += 1;
 }
-chances--;
-updateScore();
+
+chances -= 1;
+document.getElementById("result").style.display = "block";
 } else {
-document.getElementById("result").innerHTML =
-"Game Over! Final Score: Goals: " +
-goals +
-" Misses: " +
-misses +
-" <br> <button id='restartButton'>Restart</button>";
-document.getElementById("restartButton").addEventListener("click", function() {
-chances = 5;
-goals = 0;
-misses = 0;
-document.getElementById("result").innerHTML = "";
-updateScore();
+result.innerHTML = "Chances are over! Your score is " + goals + " goals and " + misses + " misses.";
+}
 });
-}
-}
-
-function updateScore() {
-document.getElementById("result").innerHTML =
-"Goals: " + goals + " Misses: " + misses + " Chances left: " + chances;
-}
-
-function getGoalkeeperChoice() {
-let choices = [
-"upleft",
-"up",
-"upright",
-"left",
-"center",
-"right",
-"downleft",
-"down",
-"downright"
-];
-return choices[Math.floor(Math.random() * choices.length)];
-}
+});
+});
 });
