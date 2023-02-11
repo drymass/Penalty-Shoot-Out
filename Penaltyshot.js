75bd19414b0b8a34ad5e6b8a535a680c443cdb20
drymass <124876608+drymass@users.noreply.github.com>
2023-02-11 22:42:19 +0800

    Update Penaltyshot.js

--- Penaltyshot.js
+++ Penaltyshot.js
@@ -86,26 +86,26 @@ endTime = new Date();
 let shotPower = (endTime - startTime) / 1000;
 let userChoice = e.target.alt;
 let goalkeeperChoice = getRandomDirection();
-result.innerHTML = `You shot ${userChoice}. The goalkeeper jumped ${goalkeeperChoice}.`;
+result.innerHTML = You shot ${userChoice}. The goalkeeper jumped ${goalkeeperChoice}.;
 if (shotPower < 0.3) {
-misses.innerHTML = `Misses: ${++totalMisses}`;
+misses.innerHTML = Misses: ${++totalMisses};
 result.innerHTML += " Under power. Miss.";
 } else if (shotPower <= 1) {
 if (userChoice === goalkeeperChoice) {
-misses.innerHTML = `Misses: ${++totalMisses}`;
+misses.innerHTML = Misses: ${++totalMisses};
 result.innerHTML += " Miss.";
 } else {
-goals.innerHTML = `Goals: ${++totalGoals}`;
+goals.innerHTML = Goals: ${++totalGoals};
 result.innerHTML += " Goal.";
 }
 } else {
-misses.innerHTML = `Misses: ${++totalMisses}`;
+misses.innerHTML = Misses: ${++totalMisses};
 result.innerHTML += " Over power. Miss.";
 }
-power.innerHTML = `Power: ${shotPower} seconds`;
+power.innerHTML = Power: ${shotPower} seconds;
 if (totalChances > 0) {
 totalChances--;
-chances.innerHTML = `Chances: ${totalChances}`;
+chances.innerHTML = Chances: ${totalChances};
 }
 if (totalChances === 0) {
 arrows.forEach(arrow => {
@@ -113,13 +113,10 @@ arrow.style.display = "none";
 });
 result.innerHTML = "Game over. Refresh the page to play again.";
 restartButton.style.display = "block";
+} else {
+arrows.forEach(arrow => {
+arrow.style.display = "block";
+});
 }
 });
-});
-chances.innerHTML = `Chances: ${totalChances}`;
-goals.innerHTML = `Goals: ${totalGoals}`;
-misses.innerHTML = `Misses: ${totalMisses}`;
-power.innerHTML = `Power:`;
-result.innerHTML = `Shoot the ball!`;
-restartButton.style.display = "none";
 });
