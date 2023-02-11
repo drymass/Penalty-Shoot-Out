b4817947823bbb60ec1be34c6811fdd38e914ac5
drymass <124876608+drymass@users.noreply.github.com>
2023-02-11 11:26:48 +0800

    Update Penaltyshot.js

--- Penaltyshot.js
+++ Penaltyshot.js
@@ -1,157 +1,119 @@
-const upleft = document.querySelector("#upleft");
-const up = document.querySelector("#up");
-const upright = document.querySelector("#upright");
-const left = document.querySelector("#left");
-const center = document.querySelector("#center");
-const right = document.querySelector("#right");
-const downleft = document.querySelector("#downleft");
-const down = document.querySelector("#down");
-const downright = document.querySelector("#downright");
-const chances = document.querySelector("#chances");
+document.addEventListener("DOMContentLoaded", function() {
+const arrows = document.querySelectorAll(".arrow");
 const result = document.querySelector("#result");
+const chances = document.querySelector("#chances");
 const goals = document.querySelector("#goals");
 const misses = document.querySelector("#misses");
+const power = document.querySelector("#power");
 const restartButton = document.querySelector("#restartButton");
-
 let totalChances = 5;
-let goalCount = 0;
-let missCount = 0;
+let totalGoals = 0;
+let totalMisses = 0;
 
-const arrows = [upleft, up, upright, left, center, right, downleft, down, downright];
-const directions = ["upleft", "up", "upright", "left", "center", "right", "downleft", "down", "downright"];
+arrows.forEach(arrow => {
+arrow.addEventListener("mousedown", e => {
+startTime = new Date();
+});
+arrow.addEventListener("mouseup", e => {
+endTime = new Date();
+let shotPower = (endTime - startTime) / 1000;
+let userChoice = e.target.alt;
+let goalkeeperChoice = getRandomDirection();
+result.innerHTML = `You shot ${userChoice}. The goalkeeper jumped ${goalkeeperChoice}.`;
 
-function chooseDirection() {
-const index = Math.floor(Math.random() * directions.length);
-return directions[index];
-}
-
-function handleClick(event) {
-const userChoice = event.target.alt;
-const goalkeeperChoice = chooseDirection();
-let shotPower;
-let missReason;
-
-const startTime = new Date();
-
-function handleClickRelease(event) {
-const endTime = new Date();
-const elapsedTime = (endTime - startTime) / 1000;
-if (elapsedTime <= 0.3) {
-missReason = "Shot was under powered.";
-shotPower = "Under powered";
-} else if (elapsedTime > 0.3 && elapsedTime <= 1) {
+if (shotPower < 0.3) {
+misses.innerHTML = `Misses: ${++totalMisses}`;
+result.innerHTML += " Under power. Miss.";
+} else if (shotPower <= 1) {
 if (userChoice === goalkeeperChoice) {
-missReason = "Goalkeeper saved the shot.";
-missCount++;
-misses.innerHTML = `Misses: ${missCount}`;
+misses.innerHTML = `Misses: ${++totalMisses}`;
+result.innerHTML += " Miss.";
 } else {
-goalCount++;
-goals.innerHTML = `Goals: ${goalCount}`;
-result.innerHTML = `Goal!`;
+goals.innerHTML = `Goals: ${++totalGoals}`;
+result.innerHTML += " Goal.";
 }
-shotPower = "Balanced";
 } else {
-missReason = "Shot was over powered.";
-shotPower = "Over powered";
+misses.innerHTML = `Misses: ${++totalMisses}`;
+result.innerHTML += " Over power. Miss.";
 }
 
-if (missReason) {
-result.innerHTML = missReason;
-misses.innerHTML = `Misses: ${missCount}`;
-totalChances--;
-}
-chances.innerHTML = `Chances: ${totalChances}`;
+power.innerHTML = `Power: ${shotPower} seconds`;
+chances.innerHTML = `Chances: ${--totalChances}`;
+
 if (totalChances === 0) {
-result.innerHTML = "Game Over! You scored " + goalCount + " goals.";
 arrows.forEach(arrow => {
-arrow.removeEventListener("mousedown", handleClick);
-arrow.removeEventListener("mouseup", handleClickRelease);
+arrow.style.display = "none";
 });
+result.innerHTML = "Game over. Refresh the page to play again.";
+restartButton.style.display = "block";
 }
-}
-
-arrow.addEventListener("mouseup", handleClickRelease);
-}
-
-arrows.forEach(arrow => {
-arrow.addEventListener("mousedown", handleClick);
+});
 });
 
-function handleClick(event) {
-const userChoice = event.target.alt;
-const goalkeeperChoice = chooseDirection();
-let shotPower;
-let missReason;
-
-const startTime = new Date();
-
-function handleClickRelease(event) {
-const endTime = new Date();
-const elapsedTime = (endTime - startTime) / 1000;
-if (elapsedTime <= 0.3) {
-missReason = "Shot was under powered.";
-shotPower = "Under powered";
-} else if (elapsedTime > 0.3 && elapsedTime <= 1) {
-if (userChoice === goalkeeperChoice) {
-missReason = "Goalkeeper saved the shot.";
-missCount++;
-misses.innerHTML = `Misses: ${missCount}`;
-} else {
-goalCount++;
-goals.innerHTML = `Goals: ${goalCount}`;
-result.innerHTML = `Goal!`;
-}
-shotPower = "Balanced";
-} else {
-missReason = "Shot was over powered.";
-shotPower = "Over powered";
+function getRandomDirection() {
+let directions = [
+"upleft",
+"up",
+"upright",
+"left",
+"center",
+"right",
+"downleft",
+"down",
+"downright"
+];
+let randomIndex = Math.floor(Math.random() * directions.length);
+return directions[randomIndex];
 }
 
-if (missReason) {
-result.innerHTML = missReason;
-misses.innerHTML = `Misses: ${missCount}`;
-totalChances--;
-}
-chances.innerHTML = `Chances: ${totalChances}`;
-if (totalChances === 0) {
-result.innerHTML = "Game Over! You scored " + goalCount + " goals.";
-arrows.forEach(arrow => {
-arrow.removeEventListener("mousedown", handleClick);
-arrow.removeEventListener("mouseup", handleClickRelease);
-});
-}
-}
-
-arrow.addEventListener("mouseup", handleClickRelease);
-}
-
-arrows.forEach(arrow => {
-arrow.addEventListener("mousedown", handleClick);
-});
-
-arrows.forEach(arrow => {
-
-arrow.addEventListener("mousedown", handleClick);
-
-arrow.addEventListener("mouseup", handleClickRelease);
-});
-
-restartButton.addEventListener("click", function() {
+restartButton.addEventListener("click", e => {
 totalChances = 5;
-goalCount = 0;
-missCount = 0;
-chances.innerHTML = `Chances: ${totalChances}`;
-goals.innerHTML = `Goals: ${goalCount}`;
-misses.innerHTML = `Misses: ${missCount}`;
-result.innerHTML = "";
+totalGoals = 0;
+totalMisses = 0;
 arrows.forEach(arrow => {
-arrow.addEventListener("mousedown", handleClick);
-arrow.addEventListener("mouseup", handleClickRelease);
+arrow.style.display = "block";
 });
-});
+chances.innerHTML = `Chances: ${totalChances}`;
+goals.innerHTML = `Goals: ${totalGoals}`;
+misses.innerHTML = `Misses: ${totalMisses}`;
 
-function initializegame(){
-document.addEventListener("DOMContentLoaded", initializegame);
+const arrows = document.querySelectorAll(".arrow");
+arrows.forEach(arrow => {
+arrow.addEventListener("mousedown", e => {
+startTime = new Date();
+});
+arrow.addEventListener("mouseup", e => {
+endTime = new Date();
+let shotPower = (endTime - startTime) / 1000;
+let userChoice = e.target.alt;
+let goalkeeperChoice = getRandomDirection();
+result.innerHTML = You shot ${userChoice}. The goalkeeper jumped ${goalkeeperChoice}.;
+
+if (shotPower < 0.3) {
+misses.innerHTML = `Misses: ${++totalMisses}`;
+result.innerHTML += " Under power. Miss.";
+} else if (shotPower <= 1) {
+if (userChoice === goalkeeperChoice) {
+misses.innerHTML = `Misses: ${++totalMisses}`;
+result.innerHTML += " Miss.";
+} else {
+goals.innerHTML = `Goals: ${++totalGoals}`;
+result.innerHTML += " Goal.";
+}
+} else {
+misses.innerHTML = `Misses: ${++totalMisses}`;
+result.innerHTML += " Over power. Miss.";
 }
 
-initializegame();
+power.innerHTML = `Power: ${shotPower} seconds`;
+chances.innerHTML = `Chances: ${--totalChances}`;
+
+if (totalChances === 0) {
+arrows.forEach(arrow => {
+arrow.style.display = "none";
+});
+result.innerHTML = "Game over. Refresh the page to play again.";
+restartButton.style.display = "block";
+}
+});
+
