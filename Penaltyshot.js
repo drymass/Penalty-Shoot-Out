093e398c5ec81dbc5e37d06d67693fe8fe80d1ec
drymass <124876608+drymass@users.noreply.github.com>
2023-02-11 16:36:20 +0800

    Update Penaltyshot.js

--- Penaltyshot.js
+++ Penaltyshot.js
@@ -49,73 +49,6 @@ restartButton.style.display = "block";
 }
 });
 });
-
-function getRandomDirection() {
-let directions = [
-"upleft",
-"up",
-"upright",
-"left",
-"center",
-"right",
-"downleft",
-"down",
-"downright"
-];
-let randomIndex = Math.floor(Math.random() * directions.length);
-return directions[randomIndex];
-}
-
-restartButton.addEventListener("click", e => {
-totalChances = 5;
-totalGoals = 0;
-totalMisses = 0;
-arrows.forEach(arrow => {
-arrow.style.display = "block";
-});
-chances.innerHTML = `Chances: ${totalChances}`;
-goals.innerHTML = `Goals: ${totalGoals}`;
-misses.innerHTML = `Misses: ${totalMisses}`;
-
-const arrows = document.querySelectorAll(".arrow");
-arrows.forEach(arrow => {
-arrow.addEventListener("mousedown", e => {
-startTime = new Date();
-});
-arrow.addEventListener("mouseup", e => {
-endTime = new Date();
-let shotPower = (endTime - startTime) / 1000;
-let userChoice = e.target.alt;
-let goalkeeperChoice = getRandomDirection();
-result.innerHTML = `You shot ${userChoice}. The goalkeeper jumped ${goalkeeperChoice}.`;
-  
-if (shotPower < 0.3) {
-misses.innerHTML = `Misses: ${++totalMisses}`;
-result.innerHTML += " Under power. Miss.";
-} else if (shotPower <= 1) {
-if (userChoice === goalkeeperChoice) {
-misses.innerHTML = `Misses: ${++totalMisses}`;
-result.innerHTML += " Miss.";
-} else {
-goals.innerHTML = `Goals: ${++totalGoals}`;
-result.innerHTML += " Goal.";
-}
-} else {
-misses.innerHTML = `Misses: ${++totalMisses}`;
-result.innerHTML += " Over power. Miss.";
-}
-
-power.innerHTML = `Power: ${shotPower} seconds`;
-chances.innerHTML = `Chances: ${--totalChances}`;
-
-if (totalChances === 0) {
-arrows.forEach(arrow => {
-arrow.style.display = "none";
-});
-result.innerHTML = "Game over. Refresh the page to play again.";
-restartButton.style.display = "block";
-}
-});
 });
 
 function getRandomDirection() {
@@ -143,6 +76,9 @@ arrow.style.display = "block";
 });
 chances.innerHTML = `Chances: ${totalChances}`;
 goals.innerHTML = `Goals: ${totalGoals}`;
-misses.innerHTML = `Misses: ${totalMisses}`;
+
+misses.innerHTML = Misses: ${totalMisses};
+power.innerHTML = Power: ;
+result.innerHTML = Shoot the ball!;
 restartButton.style.display = "none";
 });
