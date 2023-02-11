7c3eecd7d2bd0f2e6589a81949c61d0a2dea0147
drymass <124876608+drymass@users.noreply.github.com>
2023-02-11 10:57:39 +0800

    Update Penaltyshot.js

--- Penaltyshot.js
+++ Penaltyshot.js
@@ -70,6 +70,64 @@ arrow.removeEventListener("mouseup", handleClickRelease);
 }
 }
 
+arrow.addEventListener("mouseup", handleClickRelease);
+}
+
+arrows.forEach(arrow => {
+arrow.addEventListener("mousedown", handleClick);
+});
+
+function handleClick(event) {
+const userChoice = event.target.alt;
+const goalkeeperChoice = chooseDirection();
+let shotPower;
+let missReason;
+
+const startTime = new Date();
+
+function handleClickRelease(event) {
+const endTime = new Date();
+const elapsedTime = (endTime - startTime) / 1000;
+if (elapsedTime <= 0.3) {
+missReason = "Shot was under powered.";
+shotPower = "Under powered";
+} else if (elapsedTime > 0.3 && elapsedTime <= 1) {
+if (userChoice === goalkeeperChoice) {
+missReason = "Goalkeeper saved the shot.";
+missCount++;
+misses.innerHTML = `Misses: ${missCount}`;
+} else {
+goalCount++;
+goals.innerHTML = `Goals: ${goalCount}`;
+result.innerHTML = `Goal!`;
+}
+shotPower = "Balanced";
+} else {
+missReason = "Shot was over powered.";
+shotPower = "Over powered";
+}
+
+if (missReason) {
+result.innerHTML = missReason;
+misses.innerHTML = `Misses: ${missCount}`;
+totalChances--;
+}
+chances.innerHTML = `Chances: ${totalChances}`;
+if (totalChances === 0) {
+result.innerHTML = "Game Over! You scored " + goalCount + " goals.";
+arrows.forEach(arrow => {
+arrow.removeEventListener("mousedown", handleClick);
+arrow.removeEventListener("mouseup", handleClickRelease);
+});
+}
+}
+
+arrow.addEventListener("mouseup", handleClickRelease);
+}
+
+arrows.forEach(arrow => {
+arrow.addEventListener("mousedown", handleClick);
+});
 
 arrows.forEach(arrow => {
 
